806927de95bd5c78e46fedb4cff9d4c9a252346e
Maksim <mksmvnv@icloud.com>
2024-10-24 20:45:23 +0300

    Add referrers table

---
+++ src/models/referrers.py
@@ -0,0 +1,19 @@
+from uuid import uuid4
+
+from sqlalchemy import String, Boolean, UUID
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from pydantic import EmailStr
+
+from db import BaseModel
+
+
+class Referrer(BaseModel):
+    __tablename__ = "referrers"
+
+    id: Mapped[UUID] = mapped_column(UUID, primary_key=True, default=uuid4, index=True)
+    username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    email: Mapped[EmailStr] = mapped_column(String, unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+
+    referral_code: Mapped[UUID] = mapped_column(UUID, unique=True, nullable=True)
