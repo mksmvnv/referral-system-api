ef1e984379353c3b5e7411aa607d6a76034d964d
Maksim <mksmvnv@icloud.com>
2024-11-10 20:07:21 +0300

    Add test for the create referral code endpoint

--- src/tests/auth/test_register.py
+++ src/tests/auth/test_register.py
@@ -11,14 +11,16 @@ API_PREFIX = settings.api.PREFIX
 
 
 @pytest.mark.asyncio
-async def test_register_without_referral_code(async_client: AsyncClient):
+async def test_create_referral_code(async_client: AsyncClient, login_user):
+    token = await login_user("testuser", "Test12345!")
+    headers = {"Authorization": f"Bearer {token}"}
+
     response = await async_client.post(
-        f"{API_PREFIX}/referrers/register",
-        data={
-            "username": "test",
-            "email": "test@test.ru",
-            "password": "Test12345!",
-        },
+        f"{API_PREFIX}/referrers/create-referral-code", headers=headers
     )
+
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {"username": "test", "status": "success"}
+    data = response.json()
+    assert "referral_code" in data
+    assert isinstance(data["referral_code"], str)
+    assert len(data["referral_code"]) > 0
